4e8d91b0666b68d46e1890a2fb2386fe633c96c3
Jasurbek-Tursunov <jasurbek3124@icloud.com>
2024-05-21 18:42:07 +0500

    create models for management && product apps

--- management/models.py
+++ management/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Customer(models.Model):
+    user = models.OneToOneField(verbose_name="user", to=User, on_delete=models.CASCADE)
+    otp_time = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        db_table = 'customer'
+
+    def __str__(self):
+        return self.user.__str__()
+
+
+class Provider(models.Model):
+    user = models.OneToOneField(verbose_name="user", to=User, on_delete=models.CASCADE)
+
+
+class Device(models.Model):
+    """ """
+    imei = models.CharField(verbose_name="imei", max_length=16, unique=True)
+    status = models.CharField(verbose_name="Status", max_length=10, default="OFFLINE")#, choices=COMPUTING_DEVICE_STATUS)
+
+    class Meta:
+        db_table = 'device'
+        verbose_name = "Computing Device"
+        verbose_name_plural = "Computing Devices"
+
+    def __str__(self):
+        return self.imei
--- product/models.py
+++ product/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Category(models.Model):
+    """ """
+    name = models.CharField(verbose_name="Name", max_length=15)
+    slug = models.SlugField(verbose_name="Slug", max_length=15, unique=True)
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+
+class Type(models.Model):
+    """ """
+    facture = models.CharField(verbose_name="Facture")
+    duo = models.CharField(verbose_name="Duo")
+    price = models.DecimalField(verbose_name="Price", max_digits=8, decimal_places=2)
+
+
+class AbstractProduct:
+    """ """
+    category = models.ForeignKey(verbose_name="Category", to="Category", on_delete=models.CASCADE)
+    front = models.FileField(verbose_name="Front file", upload_to="./")
+    back = models.FileField(verbose_name="Back file", upload_to="./")
+    front_image = models.ImageField(verbose_name="Front image", upload_to="./")
+    back_image = models.ImageField(verbose_name="Back image", upload_to="./")
+    mockup = models.ImageField(verbose_name="Mockup", upload_to="./")
+    created_at = models.DateTimeField(verbose_name="Created time", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="Updated time", auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Template(AbstractProduct):
+
+    class Meta:
+        db_table = "template"
+        verbose_name = "Template"
+        verbose_name_plural = "Templates"
+
+
+class Product(AbstractProduct):
+    customer = models.ForeignKey(verbose_name="Customer", to="management.Customer", on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "product"
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+
+
+
+
+
